5cd091e73a826736e7c2e1b8daff5c26990bb5d8
erlams <el@gmail.com>
2023-05-15 20:01:59 +0400

    Netflix Project Loop

--- script.js
+++ script.js
@@ -8,9 +8,27 @@ const personalMovieDB = {
     private: false
 }
 
-const a = prompt('Axirinci baxdiginiz kino? ');
-const b = +prompt('Bu kinonu neche bal ile qiymetlendirirsiniz? ');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Axirinci baxdiginiz kino? ');
+    const b = +prompt('Bu kinonu neche bal ile qiymetlendirirsiniz? ');
 
-personalMovieDB.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Hazirdi');
+    } else {
+        console.log('Sehv bas verdi');
+        i--;
+    } 
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Siz film heveskarisiniz');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Siz klassik film heveskarisiniz');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Siz kinomensiniz');
+} else {
+    console.log('Sehv bas verdi');
+}
 
 console.log(personalMovieDB);
