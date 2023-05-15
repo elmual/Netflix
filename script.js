07bc787b36101d55b002805b9e2fc91e695b6b07
erlams <el@gmail.com>
2023-05-15 20:30:26 +0400

    Project Netflix Function

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt('Neche filme baxmisiniz?');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Neche filme baxmisiniz?');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Neche filme baxmisiniz?');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,27 +18,52 @@ const personalMovieDB = {
     private: false
 }
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Axirinci baxdiginiz kino? ');
-    const b = +prompt('Bu kinonu neche bal ile qiymetlendirirsiniz? ');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Axirinci baxdiginiz kino? ');
+        const b = +prompt('Bu kinonu neche bal ile qiymetlendirirsiniz? ');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Hazirdi');
+        } else {
+            console.log('Sehv bas verdi');
+            i--;
+        } 
+    }
+}
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Hazirdi');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Siz film heveskarisiniz');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Siz klassik film heveskarisiniz');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Siz kinomensiniz');
     } else {
         console.log('Sehv bas verdi');
-        i--;
-    } 
+    }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('Siz film heveskarisiniz');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Siz klassik film heveskarisiniz');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Siz kinomensiniz');
-} else {
-    console.log('Sehv bas verdi');
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
 }
 
-console.log(personalMovieDB);
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Sizin xosunuza gelen ${i}-ci janr?`);
+        personalMovieDB.genres[i-1] = genre;
+        
+    }
+}
+
+writeYourGenres();
+
